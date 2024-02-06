db7f01f10f74b07c07d855a5cb3d1586d3fdb567
sukanya <sukanya.a@chillarpayments.com>
2024-02-06 11:46:34 +0530

    db connection

---
+++ src/dbConnect.js
@@ -0,0 +1,22 @@
+
+import Sequelize from 'sequelize';
+
+const sequelize = new Sequelize('platform_doctor', 'root', '',{
+    dialect: 'mysql',
+    host: 'localhost',
+    // host: currentConfig.MYSQL_HOST,
+    // port: currentConfig.MYSQL_PORT,
+    // username: currentConfig.MYSQL_USER,
+    // password: currentConfig.MYSQL_PASSWORD,
+    // database: currentConfig.MYSQL_DATABASE,
+    logging: false, // Set to true to log SQL queries (optional)
+  });
+  
+
+// const sequelize = new Sequelize('sample', 'root', '', {
+//   host: 'localhost',
+//   dialect: 'mysql',
+// });
+  export default sequelize;
+
+  
