4feaca1dc6e1bb5f9179e1688de6ca5a28129ba8
sukanya <sukanya.a@chillarpayments.com>
2024-02-07 13:14:00 +0530

    fixes

--- src/utils/token.js
+++ src/utils/token.js
@@ -41,7 +41,7 @@ export const verifyToken =async(req,res,next)=>{
         }
     }catch(err){
         console.log({err});
-        return res.status(403).json({ statusCode:403, message:'Unauthorization' });;
+        return res.status(403).json({ statusCode:403, message:'Unauthorized' });;
     }
 }
 
