c0e159c85016b1adcfa420610bc249dc7bfbf330
linceycherian04 <lincey.cherian@chillarcards.com>
2024-02-07 10:31:43 +0530

    Updated with department routes

--- src/modules/authentication/v1/routes/authRoutes.js
+++ src/modules/authentication/v1/routes/authRoutes.js
@@ -1,10 +1,12 @@
-import authenticationController from '../controllers/authenticationController.js'
+import authenticationController from '../controllers/authenticationController.js';
+import {verifyToken} from '../../../../utils/token.js'
 
 import express from 'express';
 
 const router = express.Router();
 
 router.post("/register", authenticationController.register);
-router.post('/addProfile',authenticationController.addProfile);
+router.post('/addprofile',verifyToken,authenticationController.addProfile);
+router.post('/adddept',verifyToken,authenticationController.addDept);
 
 export default router
