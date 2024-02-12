07ca20c821a404002d109713f3c17bbb3bf5df56
sukanya <sukanya.a@chillarpayments.com>
2024-02-12 15:07:14 +0530

    total appoitments

--- src/modules/booking/v1/services/bookingService.js
+++ src/modules/booking/v1/services/bookingService.js
@@ -114,7 +114,7 @@ const listBooking = async( { doctorId, date } , res)=> {
       },
     });
 
-    console.log("weeklyTimeSlot==========", weeklyTimeSlots)
+    // console.log("weeklyTimeSlot==========", weeklyTimeSlots)
   
     if (!weeklyTimeSlots) {
       return handleResponse({
@@ -150,7 +150,7 @@ const listBooking = async( { doctorId, date } , res)=> {
             }
           }
         }
-    console.log("appointmentList", appointmentList)
+    // console.log("appointmentList", appointmentList)
   
     return handleResponse({
       res,
