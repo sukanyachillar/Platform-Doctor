caf6ca428ebac7341073c3b6f317f9b3f5a11dc5
linceycherian04 <lincey.cherian@chillarcards.com>
2024-02-13 16:48:32 +0530

    Updated with UPI ID

--- src/modules/payment/v1/services/paymentServices.js
+++ src/modules/payment/v1/services/paymentServices.js
@@ -3,6 +3,8 @@ const paymentStatusCapture = async(req,res)=>{
         console.log("body",req.body);
         console.log({orders:req.body?.payload});
         console.log({payment:req.body?.payload?.order})
+        console.log({inside:req.body?.payload?.order?.order})
+        console.log({outside:req.body?.payload?.order?.payment})
 
         return true;
 
