f7ec5f2d90a581461fcc1b8f203d3e7c4fd6a42f
linceycherian04 <lincey.cherian@chillarcards.com>
2024-02-13 18:00:36 +0530

    Updated with status

--- src/modules/payment/v1/services/paymentServices.js
+++ src/modules/payment/v1/services/paymentServices.js
@@ -1,15 +1,11 @@
 import bookingModel from '../../../../models/bookingModel.js'
 const paymentStatusCapture = async(req,res)=>{
     try{
-        console.log("body",req.body);
-        console.log({orders:req.body?.payload});
+        console.log("webhook",req.body);
         console.log({payment:req.body?.payload?.order})
-        console.log({inside:req.body?.payload?.orders?.order})
-        console.log({outside:req.body?.payload?.orders?.payment})
         if(req.body?.payload?.order){
-            console.log({status:req.body?.payload?.order?.entity.status == 'paid'})
             if(req.body?.payload?.order?.entity?.status == 'paid'){
-                console.log({statusCheck:req.body?.payload})
+               
                 let data = await bookingModel.update(
                     {
                         paymentStatus:1
