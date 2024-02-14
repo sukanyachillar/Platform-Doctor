d289ddc17ad18332863680edda58ceb4074b0352
sukanya <sukanya.a@chillarpayments.com>
2024-02-14 11:18:43 +0530

    booking status changes

--- src/modules/payment/v1/services/paymentServices.js
+++ src/modules/payment/v1/services/paymentServices.js
@@ -1,20 +1,28 @@
-import bookingModel from '../../../../models/bookingModel.js'
-const paymentStatusCapture = async(req,res)=>{
+import bookingModel from '../../../../models/bookingModel.js';
+import weeklyTimeSlotsModel from '../../../../models/weeklyTimeSlotsModel.js';
+
+const paymentStatusCapture = async(req, res)=>{
     try{
         console.log("webhook",req.body);
-        console.log({payment:req.body?.payload?.order})
+        console.log({order: req.body?.payload?.order });
+        console.log({payment:  req.body?.payload?.payment })
         if(req.body?.payload?.order){
             if(req.body?.payload?.order?.entity?.status == 'paid'){
                
                 let data = await bookingModel.update(
                     {
-                        paymentStatus:1
-                    }, {
+                        paymentStatus: 1,
+                        bookingStatus: 1,
+                        updatedAt: new Date(),
+                    }, 
+                    {
                     where: {
-                        orderId:req.body?.payload?.order?.entity?.id
+                        orderId: req.body?.payload?.order?.entity?.id
                     },
-                  })
-            }
+                  });
+
+                  await weeklyTimeSlotsModel.update({ booking_status: 1 }, { where: { time_slot_id: data.workSlotId }});
+            };
         }
 
         return true;
