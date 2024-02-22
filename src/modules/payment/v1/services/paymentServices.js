aadf025688e9ac341891ef621a23c1c935ad574c
linceycherian04 <lincey.cherian@chillarcards.com>
2024-02-22 13:01:19 +0530

    Updated with transaction history

--- src/modules/payment/v1/services/paymentServices.js
+++ src/modules/payment/v1/services/paymentServices.js
@@ -1,105 +1,136 @@
-import bookingModel from '../../../../models/bookingModel.js'
-import paymentModel from '../../../../models/paymentModel.js'
-import weeklyTimeSlotsModel from '../../../../models/weeklyTimeSlotsModel.js'
-import { handleResponse } from '../../../../utils/handlers.js'
+import bookingModel from "../../../../models/bookingModel.js";
+import paymentModel from "../../../../models/paymentModel.js";
+import weeklyTimeSlotsModel from "../../../../models/weeklyTimeSlotsModel.js";
+import { handleResponse } from "../../../../utils/handlers.js";
 
 const paymentStatusCapture = async (req, res) => {
-    try {
-        console.log('webhook', req.body)
-        console.log({ order: req.body?.payload?.order })
-        console.log({ payment: req.body?.payload?.payment })
-        if (req.body?.payload?.order) {
-            if (req.body?.payload?.order?.entity?.status == 'paid') {
-                await bookingModel.update(
-                    {
-                        // paymentStatus: 1,
-                        bookingStatus: 0,
-                        updatedAt: new Date(),
-                        // transactionId: req.body?.payload?.payment?.entity?.id,
-                    },
-                    {
-                        where: {
-                            orderId: req.body?.payload?.order?.entity?.id,
-                        },
-                    }
-                )
-                const timeSlot = await bookingModel.findOne({
-                    attributes: ['workSlotId'],
-                    where: { orderId: req.body?.payload?.order?.entity?.id },
-                })
-                await weeklyTimeSlotsModel.update(
-                    { booking_status: 1 },
-                    { where: { time_slot_id: timeSlot.workSlotId } }
-                )
-                await paymentModel.update(
-                    { 
-                        paymentStatus: 1,
-                        transactionId: req.body?.payload?.payment?.entity?.id,
-                    },
-                    { where: { orderId: req.body?.payload?.order?.entity?.id } }
-                )
-
-            }
-        }
-
-        return true
-    } catch (error) {
-        console.log({ error })
+  try {
+    console.log("webhook", req.body);
+    console.log({ order: req.body?.payload?.order });
+    console.log({ payment: req.body?.payload?.payment });
+    if (req.body?.payload?.order) {
+      if (req.body?.payload?.order?.entity?.status == "paid") {
+        await bookingModel.update(
+          {
+            // paymentStatus: 1,
+            bookingStatus: 0,
+            updatedAt: new Date(),
+            // transactionId: req.body?.payload?.payment?.entity?.id,
+          },
+          {
+            where: {
+              orderId: req.body?.payload?.order?.entity?.id,
+            },
+          }
+        );
+        const timeSlot = await bookingModel.findOne({
+          attributes: ["workSlotId"],
+          where: { orderId: req.body?.payload?.order?.entity?.id },
+        });
+        await weeklyTimeSlotsModel.update(
+          { booking_status: 1 },
+          { where: { time_slot_id: timeSlot.workSlotId } }
+        );
+        await paymentModel.update(
+          {
+            paymentStatus: 1,
+            transactionId: req.body?.payload?.payment?.entity?.id,
+          },
+          { where: { orderId: req.body?.payload?.order?.entity?.id } }
+        );
+      }
     }
-}
+
+    return true;
+  } catch (error) {
+    console.log({ error });
+  }
+};
 
 const paymentUpdate = async (bookingData, res) => {
-    try {
-        console.log({bookingData})
-        let { paymentId, orderId } = bookingData
-        await bookingModel.update(
-            {
-                // paymentStatus: 1,
-                bookingStatus: 0,
-                updatedAt: new Date(),
-                // transactionId: paymentId,
+  try {
+    console.log({ bookingData });
+    let { paymentId, orderId } = bookingData;
+    await bookingModel.update(
+      {
+        // paymentStatus: 1,
+        bookingStatus: 0,
+        updatedAt: new Date(),
+        // transactionId: paymentId,
+      },
+      {
+        where: {
+          orderId,
+        },
+      }
+    );
+    const timeSlot = await bookingModel.findOne({
+      attributes: ["workSlotId"],
+      where: { orderId },
+    });
+
+    await weeklyTimeSlotsModel.update(
+      { booking_status: 1 },
+      { where: { time_slot_id: timeSlot.workSlotId } }
+    );
+    await paymentModel.update(
+      {
+        paymentStatus: 1,
+        transactionId: paymentId,
+      },
+      { where: { orderId } }
+    );
+    return handleResponse({
+      res,
+      message: "Successfully updated with status",
+      statusCode: 200,
+    });
+  } catch (error) {
+    console.log({ error });
+    return handleResponse({
+      res,
+      message: "Unable to update status.",
+      statusCode: 404,
+    });
+  }
+};
+
+const transactionHistory = async (req, res) => {
+  try {
+    let transaction = await bookingModel.findAll({
+      where: {
+        bookingStatus: {
+          [Op.in]: [0, 1],
+        },
+        include: [
+          {
+            model: paymentModel,
+            attributes: ["orderId", "transactionId"],
+            where: {
+              paymentStatus: 0,
             },
-            {
-                where: {
-                    orderId,
-                },
-            }
-        )
-        const timeSlot = await bookingModel.findOne({
-            attributes: ['workSlotId'],
-            where: { orderId },
-        });
+          },
+        ],
+        attributes: [
+          "customerId",
+          "amount",
+          "bookingId",
+          "doctorId",
+          "bookingStatus",
+          "payment.orderId",
+          "payment.transactionId",
+        ],
+      },
+    });
+    console.log({ transaction });
+  } catch (err) {
+    console.log({ err });
+    return handleResponse({
+      res,
+      message: "Error in fetching transaction history.",
+      statusCode: 500,
+    });
+  }
+};
 
-        await weeklyTimeSlotsModel.update(
-            { booking_status: 1 },
-            { where: { time_slot_id: timeSlot.workSlotId } }
-        )
-        await paymentModel.update(
-            { 
-                paymentStatus: 1,
-                transactionId: paymentId,
-            },
-            {  where: { orderId }, }
-        )
-        return handleResponse({
-            res,
-            message: 'Successfully updated with status',
-            statusCode: 200,
-        })
-    } catch (error) {
-        console.log({ error })
-        return handleResponse({
-            res,
-            message: 'Unable to update status.',
-            statusCode: 404,
-        })
-    }
-}
-
-const transactionHistory = async(req,res)=>{
-    try{
-
-    }c
-}
-
-export default { paymentStatusCapture, paymentUpdate }
+export default { paymentStatusCapture, paymentUpdate, transactionHistory };
