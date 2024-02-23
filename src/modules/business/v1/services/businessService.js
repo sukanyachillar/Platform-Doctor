8476baa723ca2b90f70861238118fe23c4ad33a4
linceycherian04 <lincey.cherian@chillarcards.com>
2024-02-23 14:38:35 +0530

    Updated with business Model

---
+++ src/modules/business/v1/services/businessService.js
@@ -0,0 +1,76 @@
+import { handleResponse } from "../../../../utils/handlers.js";
+import businessModel from "../../../../models/businessModel.js";
+import { Op } from 'sequelize';
+
+const addBusiness = async (data, res) => {
+  try {
+    let { businessName } = data;
+    let businessData = await businessModel.findOne({ where:{businessName} });
+    console.log({businessData})
+    let message, statusCode, response, newData;
+    if (businessData) {
+      message = "Business with same name already exist.";
+      statusCode = 422;
+    } else {
+      newData = await new businessModel({ businessName });
+      response = await newData.save();
+      message = "Business registered.";
+      statusCode = 200;
+    }
+    return handleResponse({
+      res,
+      statusCode,
+      message,
+      data: {
+        response,
+      },
+    });
+  } catch (err) {
+    console.log({ err });
+    return handleResponse({
+      res,
+      message: "Error while adding data.",
+      statusCode: 404,
+    });
+  }
+};
+
+const listBusiness = async (requestData, res) => {
+  try {
+    const page = parseInt(requestData.page) || 1;
+    const pageSize = parseInt(requestData.limit) || 10;
+    const searchQuery = requestData.searchQuery || "";
+    const offset = (page - 1) * pageSize;
+    const { count, rows: records } = await businessModel.findAndCountAll({
+      where: {
+        [Op.or]: [
+          { businessName: { [Op.like]: `%${searchQuery}%` } }, // Search for businessName containing the search query
+        ],
+      },
+      attributes: ["businessName", "businessId"],
+      limit: pageSize,
+      offset: offset,
+    });
+    const totalPages = Math.ceil(count / pageSize);
+    return handleResponse({
+      res,
+      message: "Sucessfully fetched business list",
+      statusCode: 200,
+      data: {
+        records,
+        totalPages,
+        totalCount: count,
+        page,
+      },
+    });
+  } catch (error) {
+    console.log({ error });
+    return handleResponse({
+      res,
+      message: "Unable to fetch business list",
+      statusCode: 404,
+    });
+  }
+};
+
+export default { addBusiness, listBusiness };
