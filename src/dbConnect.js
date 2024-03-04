a6f855ef1f62591256e05683274b8f8ab1958b6e
sukanya <sukanya.a@chillarpayments.com>
2024-03-04 16:35:44 +0530

    fix

--- src/dbConnect.js
+++ src/dbConnect.js
@@ -5,16 +5,15 @@ const sequelize = new Sequelize('platform_doctor', process.env.MYSQL_USER, '', {
     dialect: 'mysql',
     // host: 'localhost',
     host: process.env.MYSQL_HOST,
-    port: currentConfig.MYSQL_PORT,
-    username: currentConfig.MYSQL_USER,
-    password: currentConfig.MYSQL_PASSWORD,
+    port: process.env.MYSQL_PORT,
+    username: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
     // database: currentConfig.MYSQL_DATABASE,
     logging: false, // Set to true to log SQL queries (optional)
 })
 
-// const sequelize = new Sequelize('platform_doctor', 'root', '', {
+// const sequelize = new Sequelize('sample', 'root', '', {
 //   host: 'localhost',
 //   dialect: 'mysql',
-//   logging: false,
 // });
 export default sequelize
