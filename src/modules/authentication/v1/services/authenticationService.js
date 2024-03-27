6e5ddf9f53cc5f57e24ee54471204d4d242176c5
sukanya <sukanya.a@chillarpayments.com>
2024-03-27 09:48:25 +0530

    fix

--- src/modules/authentication/v1/services/authenticationService.js
+++ src/modules/authentication/v1/services/authenticationService.js
@@ -362,7 +362,7 @@ const getProfileForCustomer = async ({ phone, encryptedPhone }, res) => {
             })
         }
          let key = userProfile?.profileImageUrl
-         const url = await awsUtils.getPresignUrlPromiseFunction(key);
+        //  const url = await awsUtils.getPresignUrlPromiseFunction(key);
 
         return handleResponse({
             res,
@@ -376,7 +376,7 @@ const getProfileForCustomer = async ({ phone, encryptedPhone }, res) => {
                 consultation_time: userProfile?.consultation_time,
                 consultation_charge: userProfile?.consultation_charge,
                 doctor_id: userProfile?.doctor_id,
-                 profileImageUrl: url,
+                //  profileImageUrl: url,
                 description: userProfile?.description,
                 uniqueDays,
                 designation: getDepartment?.department_name,
