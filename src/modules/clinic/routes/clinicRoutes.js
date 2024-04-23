256d40d30a8289b45d93c5d0dcf86f862a2c788b
sukanya <sukanya.a@chillarpayments.com>
2024-04-23 12:47:24 +0530

    added verifytoken in listing

--- src/modules/clinic/routes/clinicRoutes.js
+++ src/modules/clinic/routes/clinicRoutes.js
@@ -1,12 +1,12 @@
 import express from 'express';
 import clinicController from '../controllers/clinicController.js';
-// import { verifyAdminToken } from '../../../utils/token.js';
+import { verifyToken } from '../../../utils/token.js';
 
 const router = express.Router();
 
 router.post('/generate-otp', clinicController.generateOTP);
 router.post('/clinic-login', clinicController.clinicLogin);
-router.post('/list-booking', clinicController.listAllBooking);
+router.post('/list-booking', verifyToken, clinicController.listAllBooking);
 router.post('/booking-report', clinicController.AllBookingReport);
 
 
